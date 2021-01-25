ebf8ffe18f60e6f6271920047293479505d2688e
Jhony <jhonyquinones1104@gmail.com>
2021-01-25 15:35:49 -0500

    first game version

---
+++ Assets/Scripts/Otros/Enumeraciones.cs
@@ -0,0 +1,70 @@
+﻿public enum Direccion
+{
+    PorDefecto,
+    Arriba,
+    Abajo,
+    Izquierda,
+    Derecha
+}
+
+//FORMATO ZONA_NPC_DESCRIPCIÓN
+public enum Logro
+{
+    NINGUNO,
+    Conversacion_Candelario_1,
+    Conversacion_Candelario_2,
+    Conversacion_Candelario_Museo_Ind_1,
+    Conversacion_Candelario_Museo_Ind_2,
+    Paloma_1,
+}
+
+public enum ItemID
+{
+    NINGUNO,
+    Pokeball,
+    Pocion,
+}
+
+public enum TipoDeItem
+{
+    Combate,
+    MT,
+    MO,
+    Historia,
+    Otros,
+}
+
+public enum TipoConversacion
+{
+    Hablar,
+    Luchar
+}
+
+public enum PokemonID
+{
+    Pokemon1,
+    Pokemon2,
+}
+
+public enum AtaqueID
+{
+    Placaje,
+}
+
+public enum AtaqueElemento
+{
+    Tierra,
+    Agua,
+}
+
+public enum AtaqueTipo
+{
+    Fisico,
+    Magico
+}
+
+public enum PokedexTipoAvistamiento
+{
+    Visto,
+    Capturado
+}
